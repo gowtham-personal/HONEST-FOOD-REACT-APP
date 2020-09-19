6df2eec36d1b8b44707fa9d538cb8b962ba6a33b
Gowtham <DEV@T2Sadmins-MacBook-Air.local>
2020-09-19 20:17:58 +0530

    added api domain

--- src/constants/configConstants.js
+++ src/constants/configConstants.js
@@ -1,7 +1,6 @@
 const CONFIG_CONSTANTS = {
   GOOGLE_API_KEY: "AIzaSyDX6dB1R4OpERYO3q0hKy59h1qJ7IwJBiU",
-  // DOMAIN: "https://dev-assignment-node-api.herokuapp.com/"
-  DOMAIN: "http://localhost:3010"
+  DOMAIN: "https://honest-food-node-api.herokuapp.com"
 };
 
 export default CONFIG_CONSTANTS;
